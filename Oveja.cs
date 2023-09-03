134e451a68f896cc8894d8513df07516cdc78499
LeonardoCorbella <134092373+LeonardoCorbella@users.noreply.github.com>
2023-09-03 20:57:41 -0300

    Add files via upload

--- Oveja.cs
+++ Oveja.cs
@@ -12,13 +12,12 @@ public class Oveja : MonoBehaviour
     private float velSheep = 5.0f;
     private float dangerSheep = 5.0f;
     public GameObject target;
-    private int peligro;
-    private int bandera;
-    public float speed = 1;
+    private bool peligro;
+    private bool colision;
 
     void Start()
     {
-        bandera = 1;
+        colision = false;
         cuerpo = gameObject.GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Lobo");
     }
@@ -27,13 +26,13 @@ public class Oveja : MonoBehaviour
     {
        
         if (Vector3.Distance(transform.position, target.transform.position) > 3){
-            peligro = 0;
+            peligro = false;
         }
         else {
-            peligro = 1;
+            peligro = true;
         }
 
-        if (peligro == 0 && bandera ==1){
+        if (peligro == false && colision == false){
             Debug.Log("Modo seguro");
             cronometro += 1 * Time.deltaTime;
             if (cronometro >=1)
@@ -59,13 +58,29 @@ public class Oveja : MonoBehaviour
                     break;
             }
         }
-        else if (peligro == 1 && bandera == 1){
+        else if (peligro == true && colision == false){
             Debug.Log("Modo peligro");
             var lookPos = transform.position - target.transform.position;
             lookPos.y = 0;
             var rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
             cuerpo.AddForce(transform.forward * dangerSheep );
+        }
+         if (colision == true){
+            if(grado<180){
+                var grado2 = grado + 100;
+                var angulo2 = Quaternion.Euler(0, grado2, 0);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo2, 2);
+                cuerpo.AddForce(transform.forward * velSheep);
+                Debug.Log(grado2);
+            }
+            else{
+                var grado2 = grado - 100;
+                var angulo2 = Quaternion.Euler(0, grado2, 0);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo2, 2);
+                cuerpo.AddForce(transform.forward * velSheep);
+                Debug.Log(grado2);
+            }
         }
     }
     
@@ -80,23 +95,8 @@ public class Oveja : MonoBehaviour
     {
        
         if(collision.gameObject.tag == "Corral"){
-            bandera = 0;
-            peligro = 0;
-            var step = speed * (1+Time.deltaTime);
-            if(grado<180){
-                grado = grado + 100;
-                angulo = Quaternion.Euler(0, grado, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, step);
-                cuerpo.AddForce(transform.forward * velSheep);
-                Debug.Log("rotacion1");
-            }
-            else{
-                grado = grado - 100;
-                angulo = Quaternion.Euler(0, grado, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, step);
-                cuerpo.AddForce(transform.forward * velSheep);
-                Debug.Log("rotacion2");
-            }
+            colision = true;
+            peligro = false; 
         }
 
     }
@@ -104,7 +104,7 @@ public class Oveja : MonoBehaviour
     public void OnCollisionExit(Collision collision)
     {
         if(collision.gameObject.tag == "Corral"){
-            bandera = 1;
+            colision = false;
             Debug.Log("collision exit");
         }
     }
