ba897f9be9fe721e5caac8884d89b4feb9ebd170
Uman_dvloper <83729642+jaffreagust@users.noreply.github.com>
2023-09-08 08:37:15 -0300

    Update Oveja.cs

--- Oveja.cs
+++ Oveja.cs
@@ -5,40 +5,83 @@ using UnityEngine;
 public class Oveja : MonoBehaviour
 {
     private Rigidbody cuerpo;
+    private GameObject target;
+    private bool peligro;
+    private bool colision;
+
+    public float velSheep = 400.0f;
+    public float dangerSheep = 450.0f;
+
+    private enum SheepState { Safe, Danger, Collision };
+    private SheepState currentState = SheepState.Safe;
+
+    private Quaternion targetRotation;
+    private float rotationSpeed = 2.0f;
+
     public int rutina;
     public float cronometro;
     public Quaternion angulo;
     public float grado;
-    private float velSheep = 5.0f;
-    private float dangerSheep = 5.0f;
-    public GameObject target;
-    private bool peligro;
-    private bool colision;
+    public float maxSpeed = 500.0f; 
+    public float rotationForce= 50.0f;
+    
 
     void Start()
     {
         colision = false;
+        peligro = false;
         cuerpo = gameObject.GetComponent<Rigidbody>();
-        target = GameObject.FindGameObjectWithTag("Lobo");
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
-    public void Comportamiento_Oveja()
+    void Update()
     {
-       
-        if (Vector3.Distance(transform.position, target.transform.position) > 3)
+        Comportamiento_Oveja();
+        CheckSpeed();
+    }
+
+    void Comportamiento_Oveja()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > 15)
         {
             peligro = false;
         }
-        else 
+        else
         {
             peligro = true;
         }
-
-        if (peligro == false && colision == false)
+        
+        switch (currentState)
         {
-            Debug.Log("Modo seguro");
+            case SheepState.Safe:
+                SafeBehavior();
+                break;
+            case SheepState.Danger:
+                DangerBehavior();
+                break;
+            case SheepState.Collision:
+                CollisionBehavior();
+                break;
+        }
+
+        Debug.Log("El estado es " + currentState);
+    }
+
+    void SafeBehavior()
+    {
+        /*if (Vector3.Distance(transform.position, target.transform.position) > 15)
+        {
+            peligro = false;
+        }
+        else
+        {
+            peligro = true;
+        }*/
+
+        // Handle safe behavior here, e.g., random movement
+        Debug.Log("Modo seguro");
             cronometro += 1 * Time.deltaTime;
-            if (cronometro >=1)
+            if (cronometro >=2)
             {
                 rutina = Random.Range(0, 2);
                 cronometro = 0;
@@ -60,59 +103,59 @@ public class Oveja : MonoBehaviour
                     cuerpo.AddForce(transform.forward * velSheep);
                     break;
             }
-        }
-        else if (peligro == true && colision == false)
-        {
-            Debug.Log("Modo peligro");
-            var lookPos = transform.position - target.transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
-            cuerpo.AddForce(transform.forward * dangerSheep );
-        }
-         if (colision == true)
-         {
-            if(grado<180)
-            {
-                var grado2 = grado + 100;
-                var angulo2 = Quaternion.Euler(0, grado2, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo2, 2);
-                cuerpo.AddForce(transform.forward * velSheep);
-                Debug.Log(grado2);
-            }
-            else
-            {
-                var grado2 = grado - 100;
-                var angulo2 = Quaternion.Euler(0, grado2, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo2, 2);
-                cuerpo.AddForce(transform.forward * velSheep);
-                Debug.Log(grado2);
-            }
-        }
-    }
-    
-    void Update()
-    {
-        Comportamiento_Oveja();
+        currentState = peligro ? SheepState.Danger : SheepState.Safe;
     }
 
-    public void OnCollisionEnter(Collision collision)
+    void DangerBehavior()
     {
-       
-        if(collision.gameObject.tag == "Corral")
-        {
-            colision = true;
-            peligro = false; 
-        }
-
+        // Handle danger behavior here, e.g., flee from the player
+        var lookPos = transform.position - target.transform.position;
+        lookPos.y = 0;
+        targetRotation = Quaternion.LookRotation(lookPos) * Quaternion.Euler(0,30,0);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+        cuerpo.AddForce(transform.forward * dangerSheep);
+        currentState = peligro ? SheepState.Danger : SheepState.Safe;
     }
 
-    public void OnCollisionExit(Collision collision)
+    void CollisionBehavior()
     {
-        if(collision.gameObject.tag == "Corral")
+        // Handle collision behavior here, e.g., rotate and add force
+        // You can use your existing code for collision behavior here
+        if(grado<180)
         {
-            colision = false;
-            Debug.Log("collision exit");
+            var grado2 = grado + 100;
+            var angulo2 = Quaternion.Euler(0, grado2, 0);
+            
+            transform.Rotate(Vector3.up * 120f * rotationForce * Time.deltaTime);
+            cuerpo.AddForce(transform.forward * velSheep * 30);
+            Debug.Log(grado2);
+        }
+        else
+        {
+            var grado2 = grado - 100;
+            var angulo2 = Quaternion.Euler(0, grado2, 0);
+            transform.Rotate(Vector3.up * -120f * rotationForce * Time.deltaTime);
+            cuerpo.AddForce(transform.forward * velSheep * 30);
+            Debug.Log(grado2);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Corral")
+        {
+            currentState = SheepState.Collision;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        currentState = peligro ? SheepState.Danger : SheepState.Safe;
+    }
+    void CheckSpeed (){
+        if(cuerpo.velocity.magnitude > maxSpeed)
+        {
+               cuerpo.velocity = cuerpo.velocity.normalized * maxSpeed;
         }
     }
 }
